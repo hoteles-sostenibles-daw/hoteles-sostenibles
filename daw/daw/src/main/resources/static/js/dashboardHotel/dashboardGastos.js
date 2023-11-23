aff3ab76953895ef239992796cf09301df9fefb1
Sergi Samperi <sergi.sl.87@gmail.com>
2023-11-23 12:53:40 +0100

    cambios diseño

--- daw/daw/src/main/resources/static/js/dashboardHotel/dashboardGastos.js
+++ daw/daw/src/main/resources/static/js/dashboardHotel/dashboardGastos.js
@@ -1,3 +1,4 @@
-function insertarGasto(){
+function insertarGasto()
+{
     
 }
--- daw/daw/src/main/resources/static/js/dashboardHotel/dashboardReserva.js
+++ daw/daw/src/main/resources/static/js/dashboardHotel/dashboardReserva.js
@@ -1,23 +1,28 @@
 // obtener fecha actual
-function funcionesInicioGestionReservas(){
+function funcionesInicioGestionReservas()
+{
     fechaActual();
 }
 // componentes
-function cargarGestionReserva(){
+function cargarGestionReserva()
+{
     const numReserva = document.querySelector('.inputNumReserva').value;
     window.location.href =`http://localhost:8080/gestionreservas/reserva-${(numReserva)}`;
 }
 
-function cargarGestionGastos(){
+function cargarGestionGastos()
+{
     const numHabitacion = document.querySelector('.inputNumHabitacion').value;
     window.location.href =`http://localhost:8080/gestionreservas/habitacion-${(numHabitacion)}`;
 }
-function cargarInicio(){
+function cargarInicio()
+{
     window.location.href =`http://localhost:8080/gestionreservas`;
 }
 
 // verificar fecha aActual
-function fechaActual(){
+function fechaActual()
+{
     const spanFecha = document.querySelectorAll('.spanFecha');
     for(const span of spanFecha){
         span.textContent = `${new Date().getDate()}/${new Date().getMonth()+1}/${new Date().getFullYear()}`;
@@ -25,13 +30,15 @@ function fechaActual(){
 }
 
 //info reserva
-function funcionesInicioInfoReserva(){
+function funcionesInicioInfoReserva()
+{
     const numReserva = window.location.href.split('-')[1];
     document.querySelector('.spanNumReserva').textContent = numReserva;
 }
 
 //info reserva
-function funcionesInicioInfoGastos(){
+function funcionesInicioInfoGastos()
+{
     const numHabitacion = window.location.href.split('-')[1];
     document.querySelector('.spanNumHabitacion').textContent = numHabitacion;
 }
