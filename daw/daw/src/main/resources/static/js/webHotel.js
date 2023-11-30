1ef9c485f5e183292bb33b59789a34ab93e2ea0f
YoliB <yolandabernardezcarballo@gmail.com>
2023-11-30 10:39:59 +0100

    modificacion calendario

--- daw/daw/src/main/resources/static/js/webHotel.js
+++ daw/daw/src/main/resources/static/js/webHotel.js
@@ -1,10 +1,6 @@
-
-    
-  
-    function generarCalendario(fecha) {
-        
-        const calendarDays = document.getElementById('calendarDays');
-        const monthYear = document.getElementById('monthYear');
+  function generarCalendario(fecha) {
+      const calendarDays = document.getElementById('calendarDays');
+      const monthYear = document.getElementById('monthYear');
 
       const firstDayOfMonth = new Date(fecha.getFullYear(), fecha.getMonth(), 1);
       const lastDayOfMonth = new Date(fecha.getFullYear(), fecha.getMonth() + 1, 0);
@@ -15,7 +11,8 @@
       
       // Fill in the days of the week
       for (let i = 1; i <= 7; i++) {
-        calendarHTML += `<div class="day-cell day">${getDayName(i)}</div>`;
+       
+        calendarHTML += `<p class="diaCelda dias">${obtenerDiaSemana(i)}</p>`;
       }
   
       // Determine the day of the week for the first day of the month
@@ -25,21 +22,40 @@
       if (firstDayOfWeek === 0) {
         firstDayOfWeek = 7; // If Sunday, set it to 7
       }
-      calendarHTML += `<div class="day-cell day" style="grid-column: ${firstDayOfWeek}">${1}</div>`;
-  
-      // Fill in the rest of the days of the month
       
-      for (let i = 2; i <= lastDayOfMonth.getDate(); i++) {
+      // Fill in the rest of the days of the month
+      const fechaActual = new Date();
+      console.log(fechaActual.getDate())
 
-        if(fecha.getDay() === i){
-            console.log("hola")
-            calendarHTML += `<div class="day-cell diaActual">${i}</div>`;
+      for (let i = 2; i <= lastDayOfMonth.getDate(); i++) {
+        const currentDate = new Date(fecha.getFullYear(), fecha.getMonth(), i);
+          if(currentDate.toDateString() === fechaActual.toDateString()){
+              
+                if(fechaActual.getDate() === 1){
+                  calendarHTML += `<button class="diaCelda diaActual" type="button" style="grid-column: ${firstDayOfWeek}">1</button>`;
+                }
+                else{
+                  calendarHTML += `<button class="diaCelda diaActual" type="button">${i}</button>`;
+                }
+          }
+          else if(currentDate.toDateString() < fechaActual.toDateString()){
+           
+            if(fechaActual.getDate()=== 1){
+              calendarHTML += `<button class="diaCelda diaPasado" type="button" style="grid-column: ${firstDayOfWeek}">1</button>`;
+            }
+            else{
+              calendarHTML += `<button class="diaCelda diaPasado" type="button">${i}</button>`;
+            }
+          }
+          else{
+            if(fechaActual.getDate()=== 1){
+              calendarHTML += `<button class="diaCelda" type="button" style="grid-column: ${firstDayOfWeek}">1</button>`;
+            }
+            else{
+              calendarHTML += `<button class="diaCelda" type="button">${i}</button>`;
+            }
+          }
         }
-        else{
-            calendarHTML += `<div class="day-cell">${i}</div>`;
-        }
-      }
-  
       calendarDays.innerHTML = calendarHTML;
     }
   
@@ -48,30 +64,28 @@
       return monthNames[month];
     }
   
-    function getDayName(dayIndex) {
-      const dayNames = ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'];
-      return dayNames[dayIndex % 7];
+    function obtenerDiaSemana(diaIndex) {
+      const diaNombres = ['D', 'L', 'M', 'X', 'J', 'V', 'S'];
+      return diaNombres[diaIndex % 7];
     }
   
 
   function actualizarMes(direccion) {
-
     const mesYear = document.getElementById("monthYear").textContent;
     const year = mesYear.split(" ")[1];
     const mes = convertirMes(mesYear.split(" ")[0]);
-
-    if (direccion === 'next') {
-     const fecha = new Date (year, mes +1);
-      generarCalendario(fecha);
-    } else {
-        const fecha = new Date (year, mes -1);
-      generarCalendario(fecha);
+      if (direccion === 'next') {
+      const fecha = new Date (year, mes +1);
+        generarCalendario(fecha);
+      } else {
+          const fecha = new Date (year, mes -1);
+        generarCalendario(fecha);
+      } 
     }
-    
-  }
 
   function convertirMes(mes){
     const mesNumero = {Enero:0, Febrero:1, Marzo:2, Abril:3, Mayo:4, Junio:5, Julio:6, Agosto:7, Septiembre:8, Octubre:9, Noviembre:10, Diciembre:11};
-   console.log(mesNumero[mes]);
     return mesNumero[mes];
   }
+
+ 
