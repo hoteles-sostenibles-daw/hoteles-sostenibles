ec2157ba079c1afb56f27b188d8f6c185610a8b4
YoliB <yolandabernardezcarballo@gmail.com>
2023-12-01 09:02:32 +0100

    actualizacion calendario

--- daw/daw/src/main/resources/static/js/webHotel.js
+++ daw/daw/src/main/resources/static/js/webHotel.js
@@ -1,3 +1,5 @@
+  const intervaloFechas= [];
+
   function generarCalendario(fecha) {
       const calendarDays = document.getElementById('calendarDays');
       const monthYear = document.getElementById('monthYear');
@@ -25,26 +27,65 @@
       
       // Fill in the rest of the days of the month
       const fechaActual = new Date();
-    
+      const fechaCalendario = new Date(fecha.getFullYear(), fecha.getMonth());
       for (let i = 1; i <= lastDayOfMonth.getDate(); i++) {
-        const fechaCalendario = new Date(fecha.getFullYear(), fecha.getMonth());
+        
     
           if(i === 1)
           {
-            if(fechaActual.getDate() === 1) calendarHTML += `<button class="diaCelda diaActual" type="button" style="grid-column: ${firstDayOfWeek}">1</button>`;
+            if(fechaActual.getDate() === 1) calendarHTML += `<button class="diaCelda diaActual f${i}-${fechaCalendario.getMonth()}-${fechaCalendario.getFullYear()}" type="button" onclick = "fechaSeleccionada(this)" style="grid-column: ${firstDayOfWeek}">1</button>`;
             else if(fechaCalendario < fechaActual) calendarHTML += `<button class="diaCelda diaPasado" type="button" style="grid-column: ${firstDayOfWeek}">1</button>`; 
-            else calendarHTML += `<button class="diaCelda" type="button" style="grid-column: ${firstDayOfWeek}">1</button>`;
+            else calendarHTML += `<button class="diaCelda f${i}-${fechaCalendario.getMonth()}-${fechaCalendario.getFullYear()}" type="button" onclick = "fechaSeleccionada(this)" style="grid-column: ${firstDayOfWeek}">1</button>`;
           }
           else if(fechaCalendario.getFullYear() === fechaActual.getFullYear() && fechaCalendario.getMonth() === fechaActual.getMonth() && i === fechaActual.getDate()){
-            calendarHTML += `<button class="diaCelda diaActual" type="button">${fechaActual.getDate()}</button>`;
+            calendarHTML += `<button class="diaCelda diaActual f${i}-${fechaCalendario.getMonth()}-${fechaCalendario.getFullYear()}" type="button" onclick = "fechaSeleccionada(this)">${fechaActual.getDate()}</button>`;
           } 
           else if(fechaCalendario < fechaActual) calendarHTML += `<button class="diaCelda diaPasado" type="button">${i}</button>`;
-          else calendarHTML += `<button class="diaCelda" type="button">${i}</button>`;
+          else calendarHTML += `<button class="diaCelda f${i}-${fechaCalendario.getMonth()}-${fechaCalendario.getFullYear()}" type="button" onclick = "fechaSeleccionada(this)">${i}</button>`;
       }
       calendarDays.innerHTML = calendarHTML;
+
+      if(intervaloFechas.length === 1 || intervaloFechas.length === 2 ){
+      
+        if(intervaloFechas.length === 1){
+        
+          if(intervaloFechas[0].split("-")[1] == fechaCalendario.getMonth()){
+            const fecha1 = document.querySelector(`.f${intervaloFechas[0]}`); /* nos queda encontrar el error del querySelector */
+            console.log(intervaloFechas[0]);
+            fecha1.classList.add("fechaSeleccionada");
+          } 
+        }
+        else{
+          const fecha1 = document.querySelector(`.f${intervaloFechas[0]}`);
+          const fecha2 = document.querySelector(`.f${intervaloFechas[1]}`)
+          fecha1.classList.add("fechaSeleccionada");
+          fecha2.classList.add("fechaSeleccionada");
+        }
+        
+
+      }
     }
     
+    function fechaSeleccionada(boton){
+      const formularioReserva = document.querySelector(".formularioReserva");
+      const dia = boton.textContent;
+      const mesYear = document.getElementById("monthYear").textContent;
+      const year = mesYear.split(" ")[1];
+      const mes = convertirMes(mesYear.split(" ")[0]);
+      const fecha = `f${dia}-${mes}-${year}`;
   
+      formularioReserva.classList.remove("hidden");
+      if(intervaloFechas.length === 0){
+        intervaloFechas.push(fecha);
+        boton.classList.add("fechaSeleccionada");
+      }
+      else if(intervaloFechas.length === 1){
+        intervaloFechas.push(fecha); 
+        boton.classList.add("fechaSeleccionada");
+      }
+     
+    }
+
     function getMonthName(month) {
       const monthNames = ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
       return monthNames[month];
