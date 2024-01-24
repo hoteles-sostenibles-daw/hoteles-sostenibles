dc55ecc5cd352be91fab4d7d29a7e0c8d5f44c07
Noemi1977 <noemi.casaprima.pendas@gmail.com>
2024-01-24 11:47:42 +0100

    configuracion email desde landing

--- daw/daw/src/main/resources/static/js/landing.js
+++ daw/daw/src/main/resources/static/js/landing.js
@@ -1,9 +1,12 @@
-const url= "";
+const url= "http://localhost:8080/emailLanding";
+
+
 async function contactar(event)
 {
     event.preventDefault();
     try{
-
+        const respuesta=document.getElementById("confirmacion");
+       
         const body={
             nombre:document.querySelector(".nombre").value,
             email:document.querySelector(".email").value
@@ -19,13 +22,19 @@ async function contactar(event)
 
         if(!response.ok)
         {
+            respuesta.setAttribute("class","solicitudIncorrecta");
+            respuesta.textContent="Ha habido un error en el envío de la solicitud";
+           
             throw new Error(`Error HTTP ${response.status}`)
         }
-
-        
+        respuesta.setAttribute("class","solicitudCorrecta");
+        respuesta.textContent= "La solicitud ha sido enviada correctamente";
+       
     } 
     catch(error)
     {
+        
         console.log(error)
     }
+    
 }
