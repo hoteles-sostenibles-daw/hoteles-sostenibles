a68eade7c5a7d1a0d1ba91225dd8ccf264ec72f0
Sergi Samperi <sergi.sl.87@gmail.com>
2024-02-21 19:16:43 +0100

    cambio scripts

--- daw/daw/hoteles_sostenibles_scripts.sql
+++ daw/daw/hoteles_sostenibles_scripts.sql
@@ -3,20 +3,18 @@ USE hoteles_sostenibles_db;
 
 CREATE TABLE IF NOT EXISTS hotel (
   nombre VARCHAR(50) NOT NULL,
-  habitacionesTotales SMALLINT NOT NULL DEFAULT 0,
-  habitacionesOcupadas SMALLINT NOT NULL,
   PRIMARY KEY (nombre));
 
 CREATE TABLE IF NOT EXISTS habitacion (
   numero SMALLINT NOT NULL,
-  habitacionOcupada CHAR(1) NOT NULL,
+  habitacion_ocupada CHAR(1) NOT NULL,
   PRIMARY KEY (numero));
 
 CREATE TABLE IF NOT EXISTS huesped (
   dni VARCHAR(12) NOT NULL,
   email VARCHAR(50) NOT NULL,
   telefono VARCHAR(20) NOT NULL,
-  personaContacto VARCHAR(20) NOT NULL,
+  persona_contacto VARCHAR(20) NOT NULL,
   PRIMARY KEY (dni));
   
 CREATE TABLE IF NOT EXISTS gastos (
@@ -24,38 +22,45 @@ CREATE TABLE IF NOT EXISTS gastos (
   concepto VARCHAR(50) NOT NULL,
   pagado CHAR(1) NOT NULL,
   precio SMALLINT NOT NULL,
-  habitacionNumero SMALLINT NOT NULL,
+  habitacion_numero SMALLINT NOT NULL,
   PRIMARY KEY (id),
   CONSTRAINT fk_gastos_habitacion
-    FOREIGN KEY (habitacionNumero)
+    FOREIGN KEY (habitacion_numero)
     REFERENCES habitacion (numero)
     ON DELETE RESTRICT
     ON UPDATE CASCADE);
     
-    CREATE TABLE  IF NOT EXISTS reserva (
+  CREATE TABLE IF NOT EXISTS reserva (
   codigo VARCHAR(50) NOT NULL,
-  fechaEntrada DATE NOT NULL,
-  fechaSalida DATE NOT NULL,
+  fecha_entrada VARCHAR(20) NOT NULL,
+  fecha_salida VARCHAR(20) NOT NULL,
   check_in CHAR(1) NOT NULL,
   check_out CHAR(1) NOT NULL,
-  numeroHuespedes TINYINT NOT NULL,
-  hotelNombre VARCHAR(50) NOT NULL,
-  huespedDni VARCHAR(12) NOT NULL,
-  habitacionNumero SMALLINT NULL,
+  numero_huespedes TINYINT NOT NULL,
+  hotel_nombre VARCHAR(50) NOT NULL,
+  huesped_dni VARCHAR(12) NOT NULL,
+  habitacion_numero SMALLINT NULL,
   PRIMARY KEY (codigo),
   
   CONSTRAINT fk_reserva_hotel
-    FOREIGN KEY (hotelNombre)
+    FOREIGN KEY (hotel_nombre)
     REFERENCES hotel (nombre)
     ON DELETE RESTRICT
     ON UPDATE CASCADE,
   CONSTRAINT fk_reserva_huesped
-    FOREIGN KEY (huespedDni)
+    FOREIGN KEY (huesped_dni)
     REFERENCES huesped(dni)
     ON DELETE RESTRICT
     ON UPDATE CASCADE,
   CONSTRAINT fk_reserva_habitacion
-    FOREIGN KEY (habitacionNumero)
+    FOREIGN KEY (habitacion_numero)
     REFERENCES habitacion(numero)
     ON DELETE RESTRICT
     ON UPDATE CASCADE);
+
+  CREATE TABLE IF NOT EXISTS personal (
+  rol VARCHAR(20) NOT NULL,
+  contraseña VARCHAR(45) NOT NULL,
+  PRIMARY KEY (rol));
+
+  
